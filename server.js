12cdd4bf9308432330b645f96894815d305db69c
GABRIEL NICOLLI LOZANO LEONEL <gabriel_n_leonel@estudante.sesisenai.org.br>
2026-03-03 12:35:59 -0300

    salvar

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import fastify from 'fastify'
+import {Pool} from 'pg'
+
+const sql = new Pool({
+user: "postgres",
+password: "senai",
+host: "localhost",
+port: 5432,
+database: "receitas"
+})
 
 const servidor = fastify();
 
 servidor.get('/usuarios', () => {
     return 'funcionando!'
 })
+
+servidor.post('/usuarios', async (request, reply) => {
+    const body = request.body
+
+   await sql.query('select * from usuarios')
+
+    return resultado.rows
+})
+
+
  servidor.listen({
     port: 3000
  })
