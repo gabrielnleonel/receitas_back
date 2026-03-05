288315b1ea68f4f3e0dca81b9af6b6d24edd510d
GABRIEL NICOLLI LOZANO LEONEL <gabriel_n_leonel@estudante.sesisenai.org.br>
2026-03-05 12:25:42 -0300

    salvo

--- server.js
+++ server.js
@@ -17,9 +17,30 @@ servidor.get('/usuarios', async () => {
 })
 
 servidor.post('/usuarios', async (request, reply) => {
-    const body = request.body
+    const body = request.body;
+
+if (!body || !body.nome || !body.senha) {
+    return reply.status(400).send({
+        message:"nome ou senha obrigatórios!"
+    })
+}
+
     const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [body.nome, body.senha])
     return 'usuario cadastrado'
+})
+
+servidor.put('/usuarios/:id', async (request, reply) => {
+    const body = request.body;
+    const id = request.params.id;
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2', [body.nome, body.senha])
+    return 'usuario alterado!'
+})
+
+servidor.delete('/usuarios/:id', async (request, reply) => {
+    const id = request.params.id;
+    const resultado = await sql.query('DELETE FROM usuario where id = $1', [id])
+    console.log(resultado);
+    reply.status(200).send({message: 'usuario deletado!'})
 })
 
  servidor.listen({
